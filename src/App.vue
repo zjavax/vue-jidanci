171bb5e533fef70ecde3447a61d87e0549c2fed4
zjavax <1306187161@qq.com>
2023-04-22 10:31:15 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <el-config-provider namespace="ep">
-    <BaseHeader />
+    <!-- <BaseHeader /> -->
     <div style="display: flex">
-      <BaseSide />
+      <!-- <BaseSide /> -->
       <div>
         <!-- <img alt="Vue logo" class="element-plus-logo" src="./assets/logo.png" /> -->
         <!-- <HelloWorld msg="Hello Vue 3.0 + Element Plus + Vite" /> -->
